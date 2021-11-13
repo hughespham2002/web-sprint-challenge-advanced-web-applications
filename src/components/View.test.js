6150424a0ad91bb55e7c53cd915b2e357a17238d
Hughes <hughespham2002the2nd@yahoo.com>
2021-11-12 18:56:12 -0600

    view test 1

--- src/components/View.test.js
+++ src/components/View.test.js
@@ -3,6 +3,19 @@ import { render, screen, waitFor } from '@testing-library/react';
 import View from './View';
 
 test("renders zero articles without errors", async () => {
+    jest.mock('./../services/articleServices');
+
+test("renders zero articles without errors", async () => {
+    articleService.mockResolvedValueOnce([]);
+
+    render(<View/>);
+
+    await waitFor (() => {
+        const articles = screen.queryAllByTestId("article");
+        expect(articles).toHaveLength(0);
+        })
+
+    });
 });
 
 // test("renders three articles without errors", async ()=> {
